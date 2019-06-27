bf53a74b85fdc641317aee3616fb239acb8c64d6
varunjamwal <30648@ITCINFOTECH.COM>
2019-06-27 19:04:58 +0530

    initial

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import Styles from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />;
+        } );
+    });
     return(
         <div className={Styles.Burger}>
             <BurgerIngredient type="bread-top" />
-             <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" /> 
+             {transformedIngredients}
               <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Burger from '../../components/Burger/Burger';
 
 const BurgerBuilder = props =>{
 
+      const [BurgerState,setBurger] = useState({
+          ingredients : {
+              salad : 1,
+              bacon : 1,
+              cheese : 2,
+              meat : 2 
+          }
+      });   
     return(
         <React.Fragment>
-        <div><Burger/></div>
+        <div><Burger ingredients={BurgerState.ingredients}/></div>
         <div>Build Controls</div>
         </React.Fragment>
     );
