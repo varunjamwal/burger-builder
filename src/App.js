cd0f7d0b1b0a6f017396a408c9f2d689d6c0704d
varunjamwal <varunjamwal007@gmail.com>
2019-09-10 18:26:29 +0530

    after routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Layout from './container/Layout/Layout'
 import {Route,Switch} from 'react-router-dom'
 import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
 import Checkout from './container/Checkout/Checkout'
-
+import Orders from './container/Orders/Orders' 
 function App() {
   return (
     <div>
       <Layout>
         <Switch>
       <Route path ="/checkout" component = {Checkout} />
+      <Route path ="/orders" component = {Orders} />
         <Route path ="/" exact component = {BurgerBuilder} />
         </Switch> 
       </Layout>
--- src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Styles from './NavigationItem.module.css'
+import {NavLink} from 'react-router-dom'
 
 const navigationItem = (props) => (
     <li className={Styles.navigationItem}>
-        <a href={props.link} 
-        className={props.active ? Styles.active : null}>{props.children}</a>
+        <NavLink to={props.link} activeClassName={Styles.active} exact={props.exact}>{props.children}</NavLink>
     </li>
 );
 
